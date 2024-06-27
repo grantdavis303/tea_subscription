d9944ec24b9fb24f7efc672a78c1b848b26068f4
Grant Davis <grantdavis303@gmail.com>
2024-06-27 14:14:30 -0600

    Create New Migrations for Customer, Subscription, and Tea

--- db/migrations_ran.rb
+++ db/migrations_ran.rb
@@ -1,3 +1,6 @@
 # Migrations Ran
+rails g model Customer first_name last_name email address
+rails g model Tea name description temperature brewing_time
+rails g model Subscription title price status frequency customer:references tea:references
 
 rails db:migrate
