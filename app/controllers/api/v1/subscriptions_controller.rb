1c12388e47ad06f8eb67a2fc08fe7892107edcb8
Grant Davis <grantdavis303@gmail.com>
2024-06-29 13:50:02 -0600

    Create Index and Update Actions for Subscriptions

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::SubscriptionsController < ApplicationController
+  def index
+    customer = Customer.find(params[:user_id])
+
+    render json: SubscriptionSerializer.new.serialized_json(customer.subscriptions), status: 200
+  end
+
   def create
-    # binding.pry
     customer = Customer.find(params[:customer_id])
     tea = Tea.find(params[:tea_id])
 
@@ -18,5 +23,18 @@ class Api::V1::SubscriptionsController < ApplicationController
     # Don't create a new subscription if can't find the id of a user
     # Don't create a new subscription if can't find the id of a tea
     # Don't create a new subscription if subscription already exists
+  end
+
+  def update
+    subscription = Subscription.find_by(customer_id: params[:user_id], tea_id: params[:id])
+    subscription.update(
+      status: 'cancelled'
+    )
+
+    render json: { "success": "Subscription cancelled" }, status: 200
+
+    # Use a serializer to format a better response
+    # If subscription can't be found
+    # If subscription already exists
   end
 end
